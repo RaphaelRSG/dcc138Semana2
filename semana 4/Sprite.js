340f580ab4cae456b67f12582d3ef33f8d251e76
Rapho Rocha <54125044+RaphaelRSG@users.noreply.github.com>
2019-08-26 20:48:42 -0300

    Add files via upload
    a

---
+++ semana 4/Sprite.js
@@ -0,0 +1,29 @@
+function Sprite(params ={}){
+    var exemplo = {
+        x: 0,
+        y: 0,
+        vx: 0,
+        vy: 0,
+        h: 10,
+        w: 10,
+        color: "purple",
+        imune: 0,
+        atirando: 0,
+        comportar: undefined
+    }
+    Object.assign(this, exemplo, params);
+}
+Sprite.prototype = new Sprite();
+Sprite.prototype.constructor = Sprite;
+
+Sprite.prototype.desenhar = function (ctx){
+    ctx.fillStyle =this.color;
+    ctx.strokeStyle = "black";
+    ctx.lineWidth = 1;
+    ctx.fillRect(this.x,this.y,this.w,this.h);
+    ctx.strokeRect(this.x,this.y,this.w,this.h);    
+}
+Sprite.prototype.mover = function (dt){
+    this.x = this.x +this.vx*dt;
+    this.y = this.y +this.vy*dt;
+}
